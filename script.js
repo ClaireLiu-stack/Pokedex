fd9c8c4fddc0483d4e02e6f875cc8744a08fae0d
liu00637@algonquinlive.com <liu00637@algonquinlive.com>
2024-04-16 21:25:02 -0400

    update style.css and script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const limit = 20;
 const $pokemonGallery = document.querySelector(".pokemon-gallery");
 const $loadMoreBtn = document.getElementById("load-more");
 const $modal = document.getElementById("pokemon_modal");
+let selectedPokemonCard = null;
 let caughtPokemon = JSON.parse(localStorage.getItem("caughtPokemon")) || [];
 
 function parseUrl(url) {
@@ -19,47 +20,36 @@ async function fetchPokemon() {
     console.error("Error fetching Pokemon:", error);
   }
 }
-function toggleCatchButton(pokemonCard, pokemonName) {
-    let button = pokemonCard.querySelector('button');
-    if (!button) {
-        button = document.createElement('button');
-        button.textContent = caughtPokemon.includes(pokemonName) ? 'Release' : 'Catch';
-        button.className = 'catch-btn';
-        button.onclick = function(event) {
-            event.stopPropagation();
-            if (button.textContent === 'Catch') {
-                caughtPokemon.push(pokemonName);
-                button.textContent = 'Release';
-                pokemonCard.classList.add('caught');
-            } else {
-                caughtPokemon = caughtPokemon.filter(name => name !== pokemonName);
-                button.textContent = 'Catch';
-                pokemonCard.classList.remove('caught');
-            }
-            localStorage.setItem("caughtPokemon", JSON.stringify(caughtPokemon));
-        };
-        pokemonCard.appendChild(button);
-    }
-}
+
+// Display Pokémon in the gallery
 // Display Pokémon in the gallery
 function displayPokemon(pokemonList) {
-  pokemonList.forEach((pokemon) => {
-    const pokemonCard = document.createElement("div");
-    pokemonCard.classList.add("pokemon-card");
-    pokemonCard.dataset.name = pokemon.name; 
-    pokemonCard.innerHTML = `
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${parseUrl(
-              pokemon.url
-            )}.png" alt="${pokemon.name}">
-            <p>${pokemon.name}</p>
-        `;
-    // if (caughtPokemon.includes(pokemon.name)) {
-    //   pokemonCard.classList.add("caught");
-    // }
-    // pokemonCard.onclick = () => toggleCatchButton(pokemonCard, pokemon.name);
-    $pokemonGallery.appendChild(pokemonCard);
-  });
-}
+    pokemonList.forEach((pokemon) => {
+      const pokemonCard = document.createElement("div");
+      pokemonCard.classList.add("pokemon-card");
+      pokemonCard.dataset.name = pokemon.name;
+      pokemonCard.innerHTML = `
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${parseUrl(
+                pokemon.url
+              )}.png" alt="${pokemon.name}">
+              <p>${pokemon.name}</p>
+          `;
+      pokemonCard.classList.toggle(
+        "caught",
+        caughtPokemon.includes(pokemon.name)
+      );
+  
+      // 添加已捕捉 Pokemon 的样式
+      if (caughtPokemon.includes(pokemon.name)) {
+        const caughtOverlay = document.createElement("div");
+        caughtOverlay.classList.add("caught-overlay");
+        caughtOverlay.textContent = "CAUGHT";
+        pokemonCard.appendChild(caughtOverlay);
+      }
+      $pokemonGallery.appendChild(pokemonCard);
+    });
+  }
+  
 
 // Fetch and display Pokémon details in modal
 async function fetchPokemonDetails(pokemonName) {
@@ -74,12 +64,19 @@ async function fetchPokemonDetails(pokemonName) {
 
 // Show modal with Pokémon details
 function displayPokemonDetails(details) {
-//   console.log(details);
+  console.log(details);
+
   $modal.style.display = "flex";
   // 使用map方法提取每个对象中type对象的name属性
   const names = details.types.map((item) => item.type.name);
   // 使用join方法将提取出的name属性用逗号分隔开来，形成一个字符串
   const joinedNames = names.join(", ");
+  const isCaught = caughtPokemon.includes(details.name); //当前Pokemon card是否被捕捉
+
+  console.log(details.name);
+  console.log(isCaught);
+  console.log(caughtPokemon);
+
   $modal.innerHTML = `
         <div class="pokemon-details">
             <div class="pokemon-details-head">
@@ -88,30 +85,71 @@ function displayPokemonDetails(details) {
             </div>
             <div class="pokemon-details-footer">
                 <div class="details-footer-image">
-                    <img src="${details.sprites.other["official-artwork"].front_default}" alt="${details.name}" />
+                    <img src="${
+                      details.sprites.other["official-artwork"].front_default
+                    }" alt="${details.name}" />
                 </div>
                 <div class="details-types-moves">
                     <h3>Types</h3>
                     <p>${joinedNames}</p>
-                    <button class="catch-btn">Catch</button>
+                    <button class="catch-btn" onclick="$modal.style.display='none'">${
+                      isCaught ? "Release" : "Catch"
+                    }</button>
                 </div>
             </div>
         </div>
     `;
-  console.log(joinedNames);
-  //   console.log(details.types.map((obj) => obj.name).join(", "));
+  toggleCatchButton(selectedPokemonCard, details.name);
 }
-$loadMoreBtn.addEventListener("click", async () => {
+
+// Toggle catch/release status of a Pokémon 切换Pokemon的捕捉状态
+function toggleCatchButton(pokemonCard, pokemonName) {
+  const isCaught = caughtPokemon.includes(pokemonName);
+  const $catchButton = $modal.querySelector(".catch-btn");
+
+  $catchButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (isCaught) {
+      // Release Pokémon
+      const caughtOverlay = pokemonCard.querySelector(".caught-overlay");
+      if (caughtOverlay) {
+        caughtOverlay.remove();
+      }
+      $catchButton.textContent = "Catch";
+      $catchButton.style.borderColor = "red";
+      $catchButton.style.backgroundColor = "lightCoral";
+
+      const index = caughtPokemon.indexOf(pokemonName);
+      caughtPokemon.splice(index, 1);
+      pokemonCard.classList.remove("caught");
+    } else {
+      const caughtOverlay = document.createElement("div");
+      caughtOverlay.classList.add("caught-overlay");
+      caughtOverlay.textContent = "CAUGHT";
+      pokemonCard.appendChild(caughtOverlay);
+      $catchButton.textContent = "Release";
+      caughtPokemon.push(pokemonName);
+      pokemonCard.classList.add("caught");
+    }
+    localStorage.setItem("caughtPokemon", JSON.stringify(caughtPokemon));
+  });
+}
+
+$loadMoreBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
   offset += limit;
   await fetchPokemon();
 });
 
 // Event listener for Pokémon card click (show details)
 $pokemonGallery.addEventListener("click", (event) => {
-  const pokemonCard = event.target.closest(".pokemon-card");
+  event.preventDefault();
+  const pokemonCard = event.target.closest(".pokemon-card"); //获取当前Pokemon card的元素
   if (pokemonCard) {
+    selectedPokemonCard = pokemonCard;
     const pokemonName = pokemonCard.dataset.name;
-    fetchPokemonDetails(pokemonName);
+    fetchPokemonDetails(pokemonName); //
   }
 });
 document.getElementById("load-more").addEventListener("click", async () => {
